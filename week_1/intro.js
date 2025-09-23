1620969f711900e8691adc4e9df36287e1b94d4f
james0pritchard <james.pritch@icloud.com>
2025-09-23 11:51:14 +0100

    Initial commit

---
+++ week_1/intro.js
@@ -0,0 +1,23 @@
+// String: text, single or double quoted
+'good string'
+"good string"
+// number: whole numers and fractions
+45
+66.1
+-12
+NaN
+// boolean: true or false values. use === not ==. && means AND, || means OR.
+// undefined
+undefined
+// null 
+null
+
+// control flow - boolean/conditional operations
+const age = 4;
+if(age >= 18){ // if (boolean logic)
+    console.log("Can apply normally")
+} else if(age < 10){
+    console.log("Grow up homie")
+} else {
+    console.log("Need parental consent")
+}
